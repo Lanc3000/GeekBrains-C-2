9e571bd4cd49ed8a6acb14a21caae3aa37216a03
Timur <temurt_t@mail.ru>
2021-03-03 19:42:55 +0300

    lesson_08

--- Lesson_06_HW/MainWindow.xaml.cs
+++ Lesson_06_HW/MainWindow.xaml.cs
@@ -17,6 +17,8 @@ using System.Collections.Specialized;
 using System.Xml.Serialization;
 using System.IO;
 using Microsoft.Win32;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Lesson_06_HW
 {
@@ -25,28 +27,82 @@ namespace Lesson_06_HW
     /// </summary>
     public partial class MainWindow : Window
     {
-        ObservableCollection<string> brigades = new ObservableCollection<string>() { 
+        SqlConnection connection;
+        SqlDataAdapter adapter;
+        DataTable dt;
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog = Lesson_07; 
+                                    Integrated Security = True; Connect Timeout = 30; 
+                                    Encrypt=False;TrustServerCertificate=False;
+                                    ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            connection = new SqlConnection(connectionString);
+            adapter = new SqlDataAdapter();
+            SqlCommand command = new SqlCommand("SELECT ID, FIO, DateOfPreviousExam, EmployeeBrigade FROM Employee", connection);
+            adapter.SelectCommand = command;
+
+
+            //insert
+            command = new SqlCommand(@"INSERT INTO Employee (FIO, DateOfPreviousExam, EmployeeBrigade) VALUES (@FIO, @DateOfPreviousExam, @EmployeeBrigade); SET @ID = @@IDENTITY;", connection);
+            command.Parameters.Add("@FIO", SqlDbType.NVarChar, -1, "FIO");
+            command.Parameters.Add("@DateOfPreviousExam", SqlDbType.NVarChar, -1, "DateOfPreviousExam");
+            command.Parameters.Add("@EmployeeBrigade", SqlDbType.NVarChar, 100, "EmployeeBrigade");
+
+            SqlParameter param = command.Parameters.Add("@ID", SqlDbType.Int, 0, "ID");
+            param.Direction = ParameterDirection.Output;
+            adapter.InsertCommand = command;
+
+            // update
+            command = new SqlCommand(@"UPDATE Employee SET FIO = @FIO, DateOfPreviousExam = @DateOfPreviousExam, EmployeeBrigade = @EmployeeBrigade WHERE ID = @ID", connection);
+
+            command.Parameters.Add("@FIO", SqlDbType.NVarChar, -1, "FIO");
+            command.Parameters.Add("@DateOfPreviousExam", SqlDbType.NVarChar, -1, "DateOfPreviousExam");
+            command.Parameters.Add("@EmployeeBrigade", SqlDbType.NVarChar, 100, "EmployeeBrigade");
+
+            param = command.Parameters.Add("@ID", SqlDbType.Int, 0, "ID");
+            param.SourceVersion = DataRowVersion.Original;
+            adapter.UpdateCommand = command;
+
+            //delete
+            command = new SqlCommand("DELETE FROM Employee WHERE ID = @ID", connection);
+            param = command.Parameters.Add("@ID", SqlDbType.Int, 0, "ID");
+            param.SourceVersion = DataRowVersion.Original;
+            adapter.DeleteCommand = command;
+
+
+            dt = new DataTable();
+            adapter.Fill(dt);
+
+            dgMainList.DataContext = dt.DefaultView;
+        }
+
+        #region from XML
+        ObservableCollection<string> brigades = new ObservableCollection<string>() {
             "Бригада цифрознаковых индикаторов",
             "Бригада стабилитронов",
             "Бригада СВЧ-диодов",
             "Бригада кристаллов"
         };
         ObservableCollection<Employee> employees = new ObservableCollection<Employee>();
-        public MainWindow()
-        {
-            InitializeComponent();
-            LoadXML("..\\..\\EmployeeList.xml");
-            cbAddBrigade.SelectedIndex = 0;
-            cbAddBrigade.ItemsSource = brigades;
-            dgMainList.ItemsSource = employees;
-            dataGridComboBox.ItemsSource = brigades;
-        }
+        //public MainWindow()
+        //{
+        //    InitializeComponent();
+        //    LoadXML("..\\..\\EmployeeList.xml");
+        //    cbAddBrigade.SelectedIndex = 0;
+        //    cbAddBrigade.ItemsSource = brigades;
+        //    dgMainList.ItemsSource = employees;
+        //    dataGridComboBox.ItemsSource = brigades;
+        //}
 
         private void Open_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog dialog = new OpenFileDialog();
             dialog.Filter = "XML файл|*.XML|All files|*.*";
-            if(dialog.ShowDialog()== true)
+            if (dialog.ShowDialog() == true)
             {
                 LoadXML(dialog.FileName);
                 dgMainList.ItemsSource = employees;
@@ -57,7 +113,7 @@ namespace Lesson_06_HW
         {
             SaveFileDialog dialog = new SaveFileDialog();
             dialog.Filter = "XML файл|*.XML|All files|*.*";
-            if(dialog.ShowDialog() == true)
+            if (dialog.ShowDialog() == true)
             {
                 SaveXML(dialog.FileName);
             }
@@ -84,7 +140,8 @@ namespace Lesson_06_HW
             }
             else
             {
-                employees.Add(new Employee() { 
+                employees.Add(new Employee()
+                {
                     FIO = tbAddFIO.Text,
                     GetBrigadeName = cbAddBrigade.SelectedItem.ToString()
                 });
@@ -97,7 +154,8 @@ namespace Lesson_06_HW
 
         private void btnAddBrigade_Click(object sender, RoutedEventArgs e)
         {
-            if (tbAddNewBrigade.Text == "Название бригады" || tbAddNewBrigade.Text == "") {
+            if (tbAddNewBrigade.Text == "Название бригады" || tbAddNewBrigade.Text == "")
+            {
                 MessageBox.Show("Введите название бригады", "Ошибка ввода", MessageBoxButton.OK);
             }
             else
@@ -111,7 +169,8 @@ namespace Lesson_06_HW
         public void LoadXML(string filePath)
         {
             XmlSerializer formatter = new XmlSerializer(typeof(ObservableCollection<Employee>));
-            try {
+            try
+            {
                 using (FileStream fileStream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Read))
                 {
                     employees = (ObservableCollection<Employee>)formatter.Deserialize(fileStream);
@@ -131,5 +190,6 @@ namespace Lesson_06_HW
             }
             catch { }
         }
+        #endregion
     }
 }
